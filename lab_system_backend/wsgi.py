ce79b5cab42dd88a58e06f5744a3a8b2f24cb064
zyw <973516255@qq.com>
2022-04-22 09:20:30 +0800

    修改定时任务启动位置

--- lab_system_backend/wsgi.py
+++ lab_system_backend/wsgi.py
@@ -14,3 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab_system_backend.settings')
 
 application = get_wsgi_application()
+
+from task_tools.task_refresh_calibration_state import init_refresh_task
+from task_tools.task_remind_return import init_remind_return
+from task_tools.task_refresh_currency_rate import init_refresh_currency
+
+
+def init_uniq():
+    """
+    只初始化一次的任务
+    """
+    init_refresh_task()
+    init_remind_return()
+    init_refresh_currency()
+
+init_uniq()
